f0d05ff3c0df111d7c1a2190433b753406d4e72f
pranit95 <pranitjogwe@gmail.com>
2020-12-30 23:30:43 +0530

    My Home page

---
+++ My_Fitness_Pal_Clone/MY HOME/my-home.js
@@ -0,0 +1,46 @@
+let emailSend = document.getElementById("myhome__left_verifyEmail--link-btn")
+
+emailSend.addEventListener('click', () => {
+    emailSend.innerText = 'Email Sent!'
+    emailSend.style.backgroundColor = "rgb(250,250,250)"
+    emailSend.style.color = "rgb(185,185,185)"
+})
+
+let newsShare = document.getElementById("newsfeed__share-btn")
+
+newsShare.addEventListener('click', () => {
+    let textArea = document.getElementById("newsText")
+    if(textArea.innerHTML != ""){
+       getBlog() 
+    }
+})
+
+function getBlog(){
+
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
