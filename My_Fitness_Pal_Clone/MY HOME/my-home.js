10ecba55c9438d2ea3c3204bf79c7f0d27daeb00
pranit95 <pranitjogwe@gmail.com>
2021-01-01 22:15:13 +0530

    My Home Page final

--- My_Fitness_Pal_Clone/MY HOME/my-home.js
+++ My_Fitness_Pal_Clone/MY HOME/my-home.js
@@ -1,40 +1,3 @@
-window.addEventListener('load',executedata)
-
-
-let output = ""
-
-function executedata(){
-  let myhome = document.getElementById("myhome")
-  let addedFeed = JSON.parse(localStorage.getItem("addedFeed"))
-  if(addedFeed === null) console.log("No data is feeded")
-  else{
-    for(i in addedFeed){
-      output += ` <div class="myhome__blog_photo">
-                    
-                  </div>
-                  <div class="myhome__blog_content">
-                    <div class="blog__content">
-                        <div class="blog__content_heading">
-                            <div style="flex-basis:90%;"><a class="blog__email" style="color:rgb(0,114,188);">pranitjogwe49200</a><span> 1 hour ago</span></div>
-                            <div style="flex-basis:10%;"><span class="close" >&times;</span></div>
-                        </div>
-                        <p >${addedFeed[i].blog_info}</p>
-                        <div class="blog__content_like">
-                            <div id="like-btn"><i class="fa fa-thumbs-o-up" ></i> Like</div>
-                            <div ><i class="fa fa-comment" aria-hidden="true" style="color:rgb(137,137,137);"></i><span>  Comment</span></div>
-                        </div>
-                    </div>
-                    <div class="write__comment">
-                        <a></a> 
-                        <input type="text" placeholder="Write something..." style="width: 90%; height:30px;"></input>
-                    </div>
-                  </div>`
-    }
-
-    myBlog.innerHTML = output;
-   
-  }
-}
 
 let emailSend = document.getElementById("myhome__left_verifyEmail--link-btn")
 
@@ -45,62 +8,77 @@ emailSend.addEventListener('click', () => {
 })
 
 let newsShare = document.getElementById("newsfeed__share-btn")
-let myBlog = document.getElementById("myhome__blog") 
+let addTaskInput = document.getElementById("newsText") 
 
-
+getInfo()
 newsShare.addEventListener('click', () => {
-  
+  let addTaskInputValue = document.getElementById("newsText").value;
+  if(addTaskInputValue.trim()){
+    var task = localStorage.getItem("addedFeed");
+    if(task == null){
+      taskObj = [];
 
-  let addedFeed = JSON.parse(localStorage.getItem("addedFeed")) || []
-   addedFeed.push({blog_info: document.getElementById("newsText").value})
-   localStorage.setItem("addedFeed", JSON.stringify(addedFeed)) 
-   let added_info = JSON.parse(localStorage.getItem("addedFeed"))
-   let len = added_info.length
-   
-  function getInfo(){
-   let afterAdding = `<div class="myhome__blog_photo">
-                    <a></a>
-                  </div>
-                  <div class="myhome__blog_content">
-                    <div class="blog__content">
-                        <div class="blog__content_heading">
-                            <div style="flex-basis:90%;"><a class="blog__email" style="color:rgb(0,114,188);">pranitjogwe49200</a><span> 1 hour ago</span></div>
-                            <div style="flex-basis:10%;"><span class="close" >&times;</span></div>
-                        </div>
-                        <p >${added_info[len-1].blog_info}</p>
-                        <div class="blog__content_like">
-                            <div id="like-btn"><i class="fa fa-thumbs-o-up" ></i> Like</div>
-                            <div ><i class="fa fa-comment" aria-hidden="true" style="color:rgb(137,137,137);"></i><span>  Comment</span></div>
-                        </div>
-                    </div>
-                    <div class="write__comment">
-                        <a></a> 
-                        <input type="text" placeholder="Write something..." style="width: 90%; height:30px;"></input>
-                    </div>
-                  </div>`
-   
-    myBlog.innerHTML = output + afterAdding;
-    // document.getElementById("newsText").textContent = ""
-}
+    }else{
+      taskObj = JSON.parse(task)
+    }
 
-getInfo(added_info)
-})
+    taskObj.push({addedFeed: addTaskInputValue});
+    localStorage.setItem("addedFeed", JSON.stringify(taskObj))
+    addTaskInput.value = ""
+  } 
+  getInfo()
+});
 
-
-//     document.getElementById("like-btn").addEventListener("click", ()=>{
-//       document.getElementById("like-btn").style.color = "rgb(0,112,191)"
-//       document.getElementById("like-btn").innerHTML = " Unlike"
-    
-  
-//     })
-  
-// })
 
  
+  function getInfo(){
+    var task = localStorage.getItem("addedFeed");
+    if(task == null){
+      taskObj = [];
+    }else{
+      taskObj = JSON.parse(task)
+    }
 
-// // function getBlog(){
+    var html = ""
+    var taskList = document.getElementById("myhome__blog")
+    taskObj.forEach(function (item, index){
+      html += `<div class="myhome__blog_photo">  
+                  <div class="myhome__blog_content"  style="display:flex;">
+                      <img src="./images/photo_comment.png" style="width:60px;height:60px; margin-left:-70px">
+                      <div class="blog__content">
+                          <div class="blog__content_heading">
+                              <div style="flex-basis:90%;"><a class="blog__email" style="color:rgb(0,114,188);">pranitjogwe49200</a><span> 1 hour ago</span></div>
+                              <div style="flex-basis:10%;"><button  onclick="deleteData(${index})" class="close" >&times;</button></div>
+                          </div>
+                          <p >${item.addedFeed}</p>
+                          <div class="blog__content_like">
+                              <div id="like-btn"><i class="fa fa-thumbs-o-up" ></i> Like</div>
+                              <div ><i class="fa fa-comment" aria-hidden="true" style="color:rgb(137,137,137);"></i><span>  Comment</span></div>
+                          </div>
+                      </div>
+                  </div>
+                  <div style="width:100%; margin-bottom:20px ">
+                    <div class="write__comment">
+                      <div style="width:10%;"><img style="width:30px; height:30px; margin-left:5px;" src="./images/photo_comment.png"></div> 
+                      <textarea type="text" placeholder="Write something..." style="width: 70%; height:30px;"></textarea>
+                    </div>
+                  </div>
+              </div>`       
+    })
+   
+   
+    taskList.innerHTML = html;
+}
 
-// // }
+function deleteData(index){
+  var task = localStorage.getItem("addedFeed");
+  var taskObj = JSON.parse(task);
+  taskObj.splice(index, 1);
+  localStorage.setItem("addedFeed", JSON.stringify(taskObj))
+  getInfo();
+}
+
+
 
 // Get the modal
 var modal = document.getElementById("myModal");
