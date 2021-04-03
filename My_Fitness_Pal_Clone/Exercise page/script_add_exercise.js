31b05ce9c7f207c4976e3d8d2f2da82073331dbe
archana singh <archanasingh1643@gmail.com>
2021-04-03 18:59:55 +0530

    final changes

--- My_Fitness_Pal_Clone/Exercise page/script_add_exercise.js
+++ My_Fitness_Pal_Clone/Exercise page/script_add_exercise.js
@@ -2,7 +2,7 @@ document.getElementById("add_exercise").addEventListener("click" , function(){
     document.getElementById("exercise_show").textContent = "Exercise have been added"
     let output = "";
     output = `<div id="adding">
-    <div style="display: flex;padding-left : 60px;font-size : 15px ; font-weight : bold ;"><p style="text-align: center;">Adding:<p>${document.getElementById("exercise_input").value}</p></p></div>
+    <div style="display: flex;padding-left : 60px;font-size : 15px ; font-weight : bold ;width : 300px;"><p style="text-align: center;">Adding:<p>${document.getElementById("exercise_input").value}</p></p></div>
     <div style="display: flex;padding-left : 60px;"><p>How Long ?</p><input type="text" id="how_long"/></div>
     <div style="display: flex;padding-left : 60px;"><p>Calories Burned : </p><input type="text" id="calories_burned"/></div>
     <p>If you know how many calories you burned(eg: froem a machine at<br/> the gym), manually enter that value above</p>
